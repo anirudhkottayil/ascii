6ec575dfd96ca988024e70b44c267e9e1e6dc9df
Anirudh Kottayil Prince <anirudhkottayil@gmail.com>
2026-08-25 19:34:35 +1000

    feat: add sine to see repeated cycles based on divisor

--- stage4/pick.c
+++ stage4/pick.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 char pick(float brightness, const char* ramp){
   int n = strlen(ramp);
   int index = (int)(brightness * (n - 1) + 0.5f);
   if (index > n-1) index = n - 1;
   if (index < 0) index = 0;
-
   return ramp[index];
 }
 
 
-int main(){
-  const char* ramp = " .:-=+*#%@";
-  char c = pick(0.5, ramp);
-  printf("%c\n", c);
+void print_wave(const char *ramp, int width, float divisor) {
+    for (int col = 0; col < width; col++) {
+        // float raw = sinf(divisor / (col+1));
+        float raw = sinf(col / divisor);          // -1 to 1
+        float brightness = (raw + 1.0f) / 2.0f;    // remapped to 0 to 1
+        putchar(pick(brightness, ramp));
+    }
+    putchar('\n');
+}
 
+int main(void){
+  const char* ramp = " .:-=+*#%@";
+  print_wave(ramp, 100, 6);
   return 0;
 }
