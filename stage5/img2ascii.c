ccb4a4a2f1c546a0846726fa493be38a0f453089
Anirudh Kottayil Prince <anirudhkottayil@gmail.com>
2026-08-29 01:39:25 +1000

    feat: cache optimal image buffer loading

--- stage5/img2ascii.c
+++ stage5/img2ascii.c
@@ -33,38 +33,6 @@ int main(void){
   const char* ramp = " .:-=+*#%@";
   int n = strlen(ramp);
   GlyphBitmap glyphs[n];
-  // Load image
-  // int x,y,n;
-  // unsigned char *data = stbi_load("test1.jpg", &x, &y, &n, 0);
-  // if (data == NULL){
-  //   printf("Failed to load image\n");
-  //   return 1;
-  // }
-
-  // Build ascii img buffer
-  // int r,g,b; int idx = 0;
-  // char* buffer = malloc(((x*y) + y + 1) * sizeof(char));
-  // if (buffer == NULL){
-  //   printf("Malloc failed\n");
-  //   return 1;
-  // }
-  //
-  // for (int i = 0; i < y;i++){
-  //   for (int j = 0; j < x;){
-  //     int offset = ((i*x)+j) * n; 
-  //     r = data[offset];
-  //     g = data[offset+1];
-  //     b = data[offset+2];
-  //     float brightness = ((r + g + b) / 3.0f) / 255.0f;
-  //     char c = pick(brightness, ramp);
-  //     buffer[idx++] = c;
-  //     j += 3;
-  //   }
-  //   buffer[idx++] = '\n';
-  // }
-  // buffer[idx]= '\0';
-  // free(buffer);
-
 
   // Load font
 
@@ -94,119 +62,47 @@ int main(void){
   float scale = stbtt_ScaleForPixelHeight(&font, 32.0f);
 
   int cell = 32;
-  int x,y,channels,img_size;
+  int x,y,channels;
   unsigned char *data = stbi_load("test1.jpg", &x, &y, &channels, 0);
   if (data == NULL){
     free(ttf_buffer);
     printf("Failed to load image\n");
     return 1;
   }
-  img_size = x * y * n;
 
   // Build ascii img buffer
   int r,g,b; int idx = 0;
   int cols = x / cell;
   int rows = y / cell;
-  unsigned char* img_buffer = calloc((((rows * cols)) ), sizeof(int));
+  float* img_buffer = calloc(rows * cols , sizeof(float));
   if (img_buffer == NULL){
     stbi_image_free(data);
     free(ttf_buffer);
     printf("Malloc img_buffer failed\n");
     return 1;
   }
-  // int offset = 0;
-  // for (int row = 0; row < rows; row++){
-  //   int idx = 0;
-  //   for (int col = 0; col < cols; col++){
-  //     offset = ((row * cell* channels*x) + col*(cell*channels));
-  //     int i = 0;
-  //     while (i < cell){
-  //       r = data[offset + i];
-  //       g = data[offset + i + 1];
-  //       b = data[offset + i + 2];
-  //       img_buffer[(row*cols) + idx] += r + g + b;
-  //       i += 3;
-  //     }
-  //     idx++;
-  //   }
-  // }
-
-  for (int row = 0; row < rows; row++){
-    int idx = 0;
-    for (int col = 0; col < cols; col++){
-      int sum = 0;
-      for (int dy = 0; dy < cell; dy++){
-        int row_start = ((row*cell + dy) * x + col*cell) * channels;
-        for (int dx = 0; dx < cell; dx++){
-          int p = row_start + dx*channels;
-          r = data[p];
-          g = data[p + 1];
-          b = data[p + 2];
-          sum += r + g + b;
-        }
-      }
-      img_buffer[(row*cols) + idx] = sum / (cell*cell);
-      idx++;
+  int rc = 0; int offset = 0;
+  for (int row = 0; row < rows*cell; row++){
+    int cc = 0;
+    if ((row % cell == 0) && (row != 0)) rc++;
+    for (int col = 0; col < cols*cell*channels; col += channels){
+      if ((col % (cell*channels) == 0) && (col != 0)) cc++;
+      offset = (row * x * channels) + col;
+      r = data[offset];
+      g = data[offset + 1];
+      b = data[offset + 2];
+      img_buffer[(rc*cols) + cc] += ((r+g+b)/3.0f)/255.0f;
     }
   }
 
-  float brightness;
-  for (int i = 0; i < (rows*cols); i++){
-    brightness = (img_buffer[i]/3.0f) / 255.0f;
-    img_buffer[i] = pick(brightness, ramp);
+  for (int row = 0; row < (rows*cols); row++){
+    img_buffer[row] = pick(img_buffer[row] / (cell*cell), ramp);
   }
-  unsigned char* canvas = calloc(cell*cell*rows*cols, 1);
-  if (canvas == NULL){
-    free(img_buffer);
-    free(ttf_buffer);
-    stbi_image_free(data);
-    printf("Failed to allocate memory for canvas\n");
-    return 1;
+  for (int row = 200; row < 210; row++){
+    printf("%lf\n", img_buffer[row]);
   }
-  for (int i = 0; i < n; i++){
-    glyphs[i].pixels = stbtt_GetCodepointBitmap(&font, scale, scale, ramp[i],
-                                                  &glyphs[i].w, &glyphs[i].h,
-                                                  &glyphs[i].xoff, &glyphs[i].yoff);
-  }
-
-  int count;
-  for (int row = 0; row < rows; row++){
-    for (int col = 0; col < cols; col++){
-      int num = row*cell;
-      for (int i = 0; i < glyphs[img_buffer[row*cols+col]].h && i < cell; i++){
-        int copy_w = (glyphs[img_buffer[row*cols+col]].w < cell) ? glyphs[img_buffer[row*cols+col]].w : cell;
-        memcpy(&canvas[(num*cell*cols) + col*(cell)], &glyphs[img_buffer[row*cols+col]].pixels[i * glyphs[img_buffer[row*cols+col]].w], copy_w);
-        num +=1;
-      }
-    }
-  }
-  for (int i = 0; i < n; i++){
-    stbtt_FreeBitmap(glyphs[i].pixels, NULL);
-  }
-  stbi_write_jpg("rendered_im.jpg", cols*cell, rows*cell,1, canvas, 100);
-  free(canvas);
   free(img_buffer);
   free(ttf_buffer);
-
-  // printf("File successfully written");
-
-  // float block_w = x / width;
-  // float block_h = y / height;
-  //
-  // printf("Width: %d\n", x);
-  // printf("Height: %d\n", y);
-  // printf("Channels: %d\n", n);
-  //
-  // printf("block_w: %lf\n", block_w);
-  // printf("block_h: %lf\n", block_h);
-  //
-  // for (int i = 0; i < x; ;){
-  //   for (int j = 0; j < y; ;){
-  //
-  //   }
-  //   i += block_h;
-  // }
-
 
   stbi_image_free(data);
   return 0;
