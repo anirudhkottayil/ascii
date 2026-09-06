c4708be96400af9e751f0fe163fac28aed22eb00
Anirudh Kottayil Prince <anirudhkottayil@gmail.com>
2026-09-06 16:36:17 +1000

    fix: change compile and setup to be independent of local python version during install and make mac binary for intel and apple silicon

--- stage5/setup.py
+++ stage5/setup.py
@@ -1,8 +1,18 @@
 from setuptools import setup
 from setuptools.dist import Distribution
 
+try:
+    from setuptools.command.bdist_wheel import bdist_wheel as _bdist_wheel
+except ImportError:  # older setuptools
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+
 class BinaryDistribution(Distribution):
     def has_ext_modules(self):
         return True
+
+class bdist_wheel(_bdist_wheel):
+    def get_tag(self):
+        _, _, plat = super().get_tag()
+        return "py3", "none", plat
 
 setup(distclass=BinaryDistribution)
